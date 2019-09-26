cc3dc2ecc315e6b3f6ad76d820e944d4911e863b
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-09-26 11:54:33 -0500

    Intersection Observer

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,11 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 const video = document.querySelector("video")
 const togglePlay = document.querySelector("#togglePlay")
 const toggleMute = document.querySelector("#toggleMute")
 
-const Player = new MediaPlayer({ el: video, pluggins: [new AutoPlay] })
+const Player = new MediaPlayer({ el: video, pluggins: [new AutoPlay, new AutoPause] })
 
 togglePlay.onclick = () => Player.togglePlay()
 toggleMute.onclick = () => Player.toggleMute()
