66592273a170f11c1d2989d5a46581ad9f4fe053
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-09-26 12:19:54 -0500

    visibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,8 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25
     this.handlerIntersection = this.handlerIntersection.bind(this)
+    this.visibilityHandler = this.visibilityHandler.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
   }
   run(player) {
     this.player = player
@@ -9,17 +11,28 @@ class AutoPause {
       threshold: this.threshold
     })
     observer.observe(player.media)
+
+    document.addEventListener('visibilitychange', this.handleVisibilityChange)
+  }
+
+  visibilityHandler(isVisible) {
+    if(isVisible) {
+      this.player.play()
+    } else {
+      this.player.pause()
+    }
   }
 
 
   handlerIntersection(entries) {
     const entry = entries[0];
     const isVisible = entry.intersectionRatio >= this.threshold
-    if(isVisible) {
-      this.player.play()
-    } else {
-      this.player.pause()
-    }
+    this.visibilityHandler(isVisible)
+  }
+
+  handleVisibilityChange(){
+    const isVisible = document.visibilityState === "visible"
+    this.visibilityHandler(isVisible)
   }
 }
 
