f6bfbbde3848a58f237659ac33de5ab0fef2ccfc
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-10-06 21:57:19 -0500

    Plugin Ads en consola

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,21 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
-import AutoPause from './plugins/AutoPause.ts'
+import MediaPlayer from "./MediaPlayer.js"
+import AutoPlay from "./plugins/AutoPlay.js"
+import AutoPause from "./plugins/AutoPause.ts"
+import Ads from "./plugins/Ads"
 const video = document.querySelector("video")
 const togglePlay = document.querySelector("#togglePlay")
 const toggleMute = document.querySelector("#toggleMute")
 
-const Player = new MediaPlayer({ el: video, pluggins: [new AutoPlay, new AutoPause] })
+const Player = new MediaPlayer({
+  el: video,
+  pluggins: [new AutoPlay, new AutoPause, new Ads]
+})
 
 togglePlay.onclick = () => Player.togglePlay()
 toggleMute.onclick = () => Player.toggleMute()
 
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').catch(error => {
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").catch(error => {
     console.log(error.message)
   })
 }
