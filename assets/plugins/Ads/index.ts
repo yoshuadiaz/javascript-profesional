b15d035a80f854c30cc4e0e37c172dc34990ce7c
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-10-06 22:33:12 -0500

    Ads on screen

--- assets/plugins/Ads/index.ts
+++ assets/plugins/Ads/index.ts
@@ -6,13 +6,16 @@ class AdsPlugin {
   private player: MediaPlayer
   private media: HTMLMediaElement
   private currentAd: Ad
+  private adsContainer: HTMLElement
 
   constructor() {
     this.ads = Ads.getInstance()
+    this.adsContainer = document.createElement('div')
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this)
   }
   run(player: MediaPlayer) {
     this.player = player
+    this.player.container.appendChild(this.adsContainer)
     this.media = this.player.media
     this.media.addEventListener("timeupdate", this.handleTimeUpdate)
   }
@@ -33,7 +36,22 @@ class AdsPlugin {
     const ad = this.ads.getAd()
     this.currentAd = ad
 
-    console.log(this.currentAd)
+    this.adsContainer.innerHTML = `
+    <div class="ads">
+      <a class="ads__link" href="${this.currentAd.url}" target="_blank">
+        <img class="ads__img" src="${this.currentAd.imageUrl}" />
+        <div class="ads__info">
+          <h5 class="ads__title">${this.currentAd.title}</h5>
+          <p class="ads__body">${this.currentAd.body}</p>
+        </div>
+      </a>
+    </div>
+    `
+
+    setTimeout(() => {
+      this.currentAd = null
+      this.adsContainer.innerHTML = ''
+    }, 10000)
   }
 }
 
